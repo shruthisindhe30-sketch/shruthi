764694f3c4a7806612fb9cc9128a4b4fedd44ab0
shruthisindhe30-sketch <shruthisindhe30@gmail.com>
2026-02-10 10:16:01 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,14 +1,25 @@
 use shruthi;
 
-create table candidate(
+create table user(
 id int primary key,
 name varchar(45),
 age int,
 email varchar(45)
 );
 
-insert into candidate values(3, "maxi",20,"maxi12@gmail.com");
-insert into candidate values(5, "mini",15,"mini34@gmail.com");
-insert into candidate values(1, "mega",18,"mega56@gmail.com");
+insert into user values(3, "maxi",20,"maxi12@gmail.com");
+insert into user values(5, "mini",15,"mini34@gmail.com");
+insert into user values(1, "mega",18,"mega56@gmail.com");
 
-select * from candidate;
+select * from user;
+
+UPDATE user
+SET email = 'alice@gmail.com',
+    age = 27,
+    name = "Alice"
+WHERE id = 3;
+
+DELETE FROM user
+WHERE id = 1;
+
+
